7a388f82e353a32bff92d9b5e8e487fe256101ac
xiaolin <462441355@qq.com>
2019-02-13 11:42:39 +0800

    * 增加单元测试用例

---
+++ tests/Test/BaseTest.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xl
+ * Date: 2019/02/13
+ * Time: 上午10:47
+ */
+
+namespace Tests\Test;
+
+use Tests\TestCase;
+
+class BaseTest extends TestCase
+{
+    public function testIp()
+    {
+        $this->assertTrue(true);
+    }
+}

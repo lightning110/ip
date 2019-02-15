883c220d4c1c57508f9ebfd19453f2c4e762b372
xiaolin <462441355@qq.com>
2019-02-15 09:43:15 +0800

    * 获取ip

--- src/Ip.php
+++ src/Ip.php
@@ -8,6 +8,8 @@
 
 namespace Lin\Ip;
 
+use GuzzleHttp\Client;
+
 /**
  * 获取 ip 归属地
  * Class Ip
@@ -25,7 +27,11 @@ class Ip
     public static function find($ip)
     {
         $url = 'http://ip.taobao.com/service/getIpInfo.php?ip=' . $ip;
-        $json = file_get_contents($url);
+        //初始化 client
+        $client = new Client();
+        $json = $client->get($url)
+            ->getBody()
+            ->getContents();
         $data = json_decode($json, true);
         return $data['data'];
     }
